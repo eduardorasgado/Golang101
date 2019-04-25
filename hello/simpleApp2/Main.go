432381ec0983c436b6811351bbe1d57a87d08af8
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-04-25 07:58:47 -0500

    dojo 1

---
+++ hello/simpleApp2/Main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// coordenadas en el espacio
+	x := 45
+	y := 55
+	z := 75
+
+	/**
+	Comentario de bloque o de varias lineas:
+		- linea 1
+		- linea 2
+		- linea 3
+	*/
+	showSomething()
+}
+
+func showSomething() {
+	fmt.Printf("This is a formatter")
+}
